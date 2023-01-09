9ad912254d98927e249719b940c0b77ebff2a5d4
creampiney <sorawit12535@gmail.com>
2023-01-09 16:32:26 +0700

    feat(api): implement creating user api

--- backend/Hacky/pocketbase_api.py
+++ backend/Hacky/pocketbase_api.py
@@ -2,6 +2,20 @@ import requests
 import json
 
 
+def create_user(username, email, password, passwordConfirm, name, role):  # role : [student, instructor, admin]
+    data = {
+        "username": username,
+        "email": email,
+        "emailVisibility": True,
+        "password": password,
+        "passwordConfirm": passwordConfirm,
+        "name": name,
+        "role": role
+    }
+    response = requests.post('https://pb.jjus.dev/api/collections/users/records', json=data)
+    return response.json()
+
+
 def get_all_courses(sort='', filter=''):
     response = requests.get('https://pb.jjus.dev/api/collections/courses/records?sort='+sort+'&filter='+filter)
     return response.json()
