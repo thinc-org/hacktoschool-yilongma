2bfc5a6d4d49ef1ac47be4122e63c61ae963f0b1
creampiney <sorawit12535@gmail.com>
2023-01-10 06:58:39 +0700

    fix: reimplement main.tsx

---
+++ frontend-tsc/src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import App from './App'
+import HeaderV3 from './components/Header/HeaderV3'
+import Home from './components/Homepage/Home'
+import Login from './components/Auth/Login'
+import Register from './components/Register/Register'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<><HeaderV3/><Home/></>} />
+          <Route path='/login' element={<><Login/></>} />
+          <Route path='/register' element={<><Register/></>} />
+        </Routes>
+      </BrowserRouter>
+  </React.StrictMode>,
+)
