8e566afc15bf62a2b849c0d784f8b6517d6566ad
creampiney <sorawit12535@gmail.com>
2023-01-09 15:55:39 +0700

    feat(api): implement courses api

--- backend/Hacky/main.py
+++ backend/Hacky/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import json
 from slack_notification import *
+from pocketbase_api import *
 
 
 app = Flask(__name__)
@@ -11,8 +12,28 @@ def hello_world():
     return {"message": "Hello!"}, 201
 
 
+@app.route('/courses', methods=["POST", "GET"])
+def courses():
+    if (request.method == 'GET'):
+        return get_all_courses()
+    else:
+        body = request.get_json()
+        return create_course(body['name'], body['instructor_id'])
+
+
+@app.route('/courses/<string:id>', methods=["GET", "PUT", "DELETE"])
+def course(id):
+    if (request.method == 'GET'):
+        return get_course_by_id(id)
+    elif (request.method == 'DELETE'):
+        return delete_course_by_id(id)
+    else:
+        return 0
+
+
 @app.route('/notification/slack', methods=["POST"])
 def slack_noti():
+
     record = json.loads(request.data)
     try:
         send_notification_slack(record['msg'])
