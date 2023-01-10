2e7f9ea7184f14df2fdd689b0122b8d0c636d686
JustinJira <jus.jira@gmail.com>
2023-01-10 17:22:31 +0700

    feat: added footer in homepage

--- frontend-tsc/src/main.tsx
+++ frontend-tsc/src/main.tsx
@@ -4,9 +4,11 @@ import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom'
 import App from './App'
 import HeaderV3 from './components/Header/HeaderV3'
 import Home from './components/Homepage/Home'
+import Footer from './components/Footer/Footer'
 import Login from './components/Auth/Login'
 import Register from './components/Register/Register'
 import CoursesList from './components/CoursesList/CoursesList'
+
 import './index.css'
 
 
@@ -14,11 +16,11 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
         <Routes>
-          <Route path='/' element={<><HeaderV3/><Home/></>} />
+          <Route path='/' element={<><HeaderV3/><Home/><Footer/></>} />
           <Route path='/login' element={<><Login/></>} />
           <Route path='/register' element={<><Register/></>} />
           <Route path='/courses' element={<><HeaderV3/><CoursesList/></>} />
-          <Route path='*' element={<Navigate to="/" />} />
+          {/* <Route path='*' element={<Navigate to="/" />} /> */}
         </Routes>
       </BrowserRouter>
   </React.StrictMode>,
