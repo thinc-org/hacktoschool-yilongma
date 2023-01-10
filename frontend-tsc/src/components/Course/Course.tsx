757b712d27bc35d1bb6e2733cc3807f3d97fdc4c
creampiney <sorawit12535@gmail.com>
2023-01-10 21:31:53 +0700

    feat: student list in course page for instructor

--- frontend-tsc/src/components/Course/Course.tsx
+++ frontend-tsc/src/components/Course/Course.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import PocketBase from 'pocketbase'
+import StudentList from './StudentList';
 
 const pb = new PocketBase('https://pb.jjus.dev');
 
@@ -12,13 +13,14 @@ const Course = () => {
         "name": "",
         "expand": {
             "instructor": {},
+            "student": []
         },
         "description": ""
     });
 
     const getCourseData = async () => {
         const record = await pb.collection('courses').getOne(id || "", {
-            expand: 'instructor',
+            expand: 'instructor,student',
         });
         console.log(record)
         setCourseData(record)
@@ -35,14 +37,15 @@ const Course = () => {
                     <img src="https://picsum.photos/200/300" className='h-64 w-64 rounded-l-lg min-h-64 max-h-64 min-w-64 max-w-64' />
                     <div className='flex flex-col'>
                         <p className="font-['DelaGothicOne'] text-[1.2rem] px-8 py-4 overflow-hidden whitespace-pre-line">{courseData.name}</p>
-                        <p className="font-['Montserrat'] text-[1rem] px-8 py-2 font-bold overflow-hidden whitespace-pre-line">Instructor: {courseData.expand.instructor.name}</p>
-                        <p className="font-['Montserrat'] text-[1rem] px-8 py-2 overflow-hidden whitespace-pre-line">Description: {courseData.description}</p>
+                        <p className="font-['Montserrat'] text-[1rem] px-8 py-3 font-bold overflow-hidden whitespace-pre-line">Instructor: {courseData.expand.instructor.name}</p>
+                        <p className="font-['Montserrat'] text-[1rem] px-8 py-2 overflow-auto whitespace-pre-line">Description: {courseData.description}</p>
                         <div className="flex flex-col px-8 py-2">
                             <button className="bg-[#639B6D] hover:bg-[#74bf81] w-40 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline">Enroll</button>
                         </div>
 
                     </div>
                 </div>
+                <StudentList data={courseData}/>
             </div>
         </div>
         
