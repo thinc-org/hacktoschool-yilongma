237ef001dc1b54c70102d038cad052521b21919e
creampiney <sorawit12535@gmail.com>
2023-01-10 13:54:39 +0700

    feat: implement course box in courses list

--- frontend-tsc/src/components/CoursesList/CoursesList.tsx
+++ frontend-tsc/src/components/CoursesList/CoursesList.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import PocketBase from 'pocketbase'
+import CourseBox from './CourseBox';
 
 
 const pb = new PocketBase('https://pb.jjus.dev');
@@ -24,28 +25,18 @@ function CoursesList() {
 
 
     return (
-        <div className='max-w-screen h-screen bg-[#F6F5F4]'>
-            <div className='flex md:flex-row gap-16 md:p-12 md:px-24 lg:p-20 lg:px-48  xl:p-32 xl:px-72'>
+        <div className='max-w-screen min-h-screen bg-[#F6F5F4]'>
+            <div className='flex flex-col md:p-12 md:px-24 lg:p-12 lg:px-48  xl:p-12 xl:px-48'>
                 <div className='flex flex-col w-full justify-center'>
-                    <form className="flex justify-center ">   
-                        <label className="sr-only">Search</label>
-                        <div className="relative w-full">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                            </div>
-                            <input type="text" id="search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
-                        </div>
-                        <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                            <span className="sr-only">Search</span>
-                        </button>
-                    </form>
+                    <div className="flex justify-center ">   
+                        <input type="text" id="search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search"/>
+                    </div>
                 </div>
 
                 <div>
-                    <div className=''>
-
-                    </div>
+                    <CourseBox/>
+                    <CourseBox/>
+                    <CourseBox/>
                 </div>
             
             </div>
