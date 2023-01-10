0b7663378cc76f1bd8fa1e800deaa48ac8ee7a47
JustinJira <jus.jira@gmail.com>
2023-01-10 23:40:23 +0700

    style: responsive design

--- frontend-tsc/src/components/Course/Course.tsx
+++ frontend-tsc/src/components/Course/Course.tsx
@@ -9,7 +9,7 @@ const pb = new PocketBase('https://pb.jjus.dev');
 
 const Course = () => {
     let { id } = useParams();
-    let navigate = useNavigate(); 
+    let navigate = useNavigate();
     const cookie = Cookies.get('token')
 
     if (!cookie) {
@@ -39,35 +39,51 @@ const Course = () => {
     }
 
     useEffect(() => {
-        
+
         getCourseData();
     }, [])
 
     return (
-        <div className='max-w-screen min-h-screen bg-[#F6F5F4]'>
+        <div className='max-w-screen min-h-screen bg-[#F6F5F4 flex items-center md:items-start'>
             <div className='flex flex-col md:p-12 md:px-24 lg:p-12 lg:px-48  xl:p-12 xl:px-48'>
-                <div className='flex flex-row rounded-lg bg-[#FFFFFF] h-64 mt-8 mb-8 shadow hover:shadow-lg'>
-                    <img src="https://picsum.photos/200/300" className='h-64 w-64 rounded-l-lg min-h-64 max-h-64 min-w-64 max-w-64' />
-                    <div className='flex flex-col'>
+                <div className="hidden md:grid grid-cols-[20vw_1fr] rounded-lg bg-[#FFFFFF] h-64 mt-8 mb-8 shadow hover:shadow-lg">
+                    <img src="https://picsum.photos/200/300" className='w-full h-full rounded-l-lg min-h-64 max-h-64 min-w-64 max-w-64' />
+                    <div className='flex flex-col max-h-64'>
                         <p className="font-['DelaGothicOne'] text-[1.2rem] px-8 py-4 overflow-hidden whitespace-pre-line">{courseData.name}</p>
                         <p className="font-['Montserrat'] text-[1rem] px-8 py-3 font-bold overflow-hidden whitespace-pre-line">Instructor: {courseData.expand.instructor.name}</p>
-                        <p className="font-['Montserrat'] text-[1rem] px-8 py-2 overflow-auto whitespace-pre-line">{courseData.description}</p>
+                        <p className="font-['Montserrat'] text-[1rem] px-8 py-2 overflow-scroll whitespace-pre-line">{courseData.description}</p>
                         <div className="flex flex-col px-8 py-2">
                             {
-                                ((pb.authStore.model!.role).includes('student'))?
-                                    (!courseData.student.includes(pb.authStore.model!.id)?
-                                        <button className="bg-[#639B6D] hover:bg-[#74bf81] w-40 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline">Enroll</button>:
-                                        <button className="bg-[#585858] w-40 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline disabled">Enrolled</button>):
+                                ((pb.authStore.model!.role).includes('student')) ?
+                                    (!courseData.student.includes(pb.authStore.model!.id) ?
+                                        <button className="bg-[#639B6D] hover:bg-[#74bf81] w-40 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline">Enroll</button> :
+                                        <button className="bg-[#585858] w-40 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline disabled">Enrolled</button>) :
                                     ""
                             }
                         </div>
-
                     </div>
                 </div>
-                {pb.authStore.model!.role.includes('instructor')?<StudentList data={courseData}/>:""}
+                <div className="md:hidden grid grid-rows-[h-64_1fr] rounded-lg bg-[#FFFFFF] h-fit shadow hover:shadow-lg m-8">
+                    <img src="https://picsum.photos/200/300" className='w-full h-full object-fill rounded-t-lg min-h-64 max-h-64 min-w-64 max-w-64' />
+                    <div className='flex flex-col m-4'>
+                        <p className="font-['DelaGothicOne'] text-[1.2rem] px-8 py-0 overflow-hidden whitespace-pre-line">{courseData.name}</p>
+                        <p className="font-['Montserrat'] text-[1rem] px-8 py-3 font-bold overflow-hidden whitespace-pre-line">Instructor: {courseData.expand.instructor.name}</p>
+                        <p className="font-['Montserrat'] text-[1rem] px-8 py-2 overflow-scroll whitespace-pre-line">{courseData.description}</p>
+                        <div className="flex flex-col px-8 py-2 items-center">
+                            {
+                                ((pb.authStore.model!.role).includes('student')) ?
+                                    (!courseData.student.includes(pb.authStore.model!.id) ?
+                                        <button className="bg-[#639B6D] hover:bg-[#74bf81] w-40 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline">Enroll</button> :
+                                        <button className="bg-[#585858] w-40 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline disabled">Enrolled</button>) :
+                                    ""
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
+            {pb.authStore.model!.role.includes('instructor') ? <StudentList data={courseData} /> : ""}
         </div>
-        
+
     )
 
 }
--- frontend-tsc/src/components/CoursesList/CourseBox.tsx
+++ frontend-tsc/src/components/CoursesList/CourseBox.tsx
@@ -15,7 +15,7 @@ const CourseBox = ({ id, name, instructor }: { id: string; name: string; instruc
     return (
         <>
             <div className="hidden md:grid grid-cols-[20vw_1fr] rounded-lg bg-[#FFFFFF] h-fit mt-8 mb-8 shadow hover:shadow-lg">
-                <img src="https://picsum.photos/200/300" className='w-full h-full object-fill rounded-l-lg min-h-64 max-h-64 min-w-64 max-w-64' />
+                <img src="https://picsum.photos/200/300" className='aspect-[3/2] w-full h-full object-fill rounded-l-lg min-h-64 max-h-64 min-w-64 max-w-64' />
                 <div className='relative flex flex-col flex-shrink min-w-0 items-center md:items-start px-4 py-8 md:px-8 md:py-4 gap-2 md:gap-6'>
                     <p className="font-['DelaGothicOne'] text-sm md:text-[1.2rem] overflow-hidden whitespace-pre-line">{name}</p>
                     <p className="font-['Montserrat'] text-sm md:text-[1rem] font-bold overflow-hidden whitespace-pre-line">Instructor: {instructor}</p>
@@ -23,7 +23,7 @@ const CourseBox = ({ id, name, instructor }: { id: string; name: string; instruc
                 </div>
             </div>
             <div className="md:hidden grid grid-rows-2 rounded-lg bg-[#FFFFFF] h-fit mt-8 mb-8 shadow hover:shadow-lg m-8">
-                <img src="https://picsum.photos/200/300" className='w-full h-full object-fill rounded-t-lg min-h-64 max-h-64 min-w-64 max-w-64' />
+                <img src="https://picsum.photos/300/200" className='aspect-[2/3] w-full h-full rounded-t-lg min-h-64 max-h-64 min-w-64 max-w-64' />
                 <div className='relative flex flex-col flex-shrink min-w-0 items-center md:items-start px-4 py-8 md:px-8 md:py-4 gap-2 md:gap-6'>
                     <p className="font-['DelaGothicOne'] text-sm md:text-[1.2rem] overflow-hidden whitespace-pre-line">{name}</p>
                     <p className="font-['Montserrat'] text-sm md:text-[1rem] font-bold overflow-hidden whitespace-pre-line">Instructor: {instructor}</p>
