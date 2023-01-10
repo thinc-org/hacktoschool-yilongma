737fc5db85c4a30d542380d85d88f177c876bf3b
creampiney <sorawit12535@gmail.com>
2023-01-10 23:34:38 +0700

    feat: implement course adder

---
+++ frontend-tsc/src/components/CoursesList/CourseAdder.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+const CourseAdder = () => {
+
+
+
+    return (
+        <div className='flex flex-row rounded-lg bg-[#FFFFFF] h-64 mt-8 mb-8 shadow hover:shadow-lg'>
+            
+        </div>
+    )
+
+}
+
+export default CourseAdder
