fc4ed1f9b8462d9ed99eeea25b62d3af1a62af34
creampiney <sorawit12535@gmail.com>
2023-01-11 10:44:52 +0700

    feat: implement announcements

--- frontend-tsc/src/components/Course/Course.tsx
+++ frontend-tsc/src/components/Course/Course.tsx
@@ -5,6 +5,7 @@ import StudentList from './StudentList'
 import Cookies from 'js-cookie'
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import Announcement from './Announcement'
 
 const pb = new PocketBase('https://pb.jjus.dev');
 
@@ -34,7 +35,7 @@ const Course = () => {
 
     const getCourseData = async () => {
         const record = await pb.collection('courses').getOne(id || "", {
-            expand: 'instructor,student',
+            expand: 'instructor,student,announcement',
         });
         console.log(record)
         setCourseData(record)
@@ -92,7 +93,7 @@ const Course = () => {
     },[])
 
     return (
-        <div className='max-w-screen min-h-screen bg-[#F6F5F4 flex flex-col items-center md:items-start'>
+        <div className='max-w-screen min-h-screen bg-[#F6F5F4] flex flex-col items-center md:items-start'>
             <div className='min-w-full flex flex-col md:p-12 md:px-24 lg:p-12 lg:px-48  xl:p-12 xl:px-48'>
                 <div className="hidden md:grid grid-cols-[20vw_1fr] rounded-lg bg-[#FFFFFF] h-64 mt-8 mb-8 shadow hover:shadow-lg">
                     <img src="https://picsum.photos/200/300" className='w-full h-full rounded-l-lg min-h-64 max-h-64 min-w-64 max-w-64' />
@@ -111,7 +112,7 @@ const Course = () => {
                         </div>
                     </div>
                 </div>
-                <div className="max-h-[40rem] md:hidden grid grid-rows-[h-64_1fr] rounded-lg bg-[#FFFFFF] h-fit shadow hover:shadow-lg m-8">
+                <div className="max-h-[43rem] md:hidden grid grid-rows-[h-64_1fr] rounded-lg bg-[#FFFFFF] h-fit shadow hover:shadow-lg m-8">
                     <img src="https://picsum.photos/200/300" className='w-full h-full object-fill rounded-t-lg min-h-64 max-h-64 min-w-64 max-w-64' />
                     <div className='flex flex-col m-4'>
                         <p className="font-['DelaGothicOne'] text-[1.2rem] px-8 py-0 overflow-hidden whitespace-pre-line">{courseData.name}</p>
@@ -132,6 +133,7 @@ const Course = () => {
                     </div>
                 </div>
                 {pb.authStore.model!.role.includes('instructor') ? <StudentList data={courseData} /> : ""}
+                {pb.authStore.model!.role.includes('student')&&courseData.student.includes(pb.authStore.model!.id) ? <Announcement data={courseData} /> : ""}
             </div>
             
         </div>
