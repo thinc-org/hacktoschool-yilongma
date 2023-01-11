248c425955eefa1b48b6d2b3ba81a1149f80a230
creampiney <sorawit12535@gmail.com>
2023-01-11 11:55:21 +0700

    feat: implement announcement adder ui

--- frontend-tsc/src/components/Course/Announcement.tsx
+++ frontend-tsc/src/components/Course/Announcement.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import AnnouncementAdder from './Adder/AnnouncementAdder';
+import PocketBase from 'pocketbase';
 
+const pb = new PocketBase('https://pb.jjus.dev');
 
 const Announcement = ({ data }:{data:any;}) => {
 
@@ -24,6 +27,9 @@ const Announcement = ({ data }:{data:any;}) => {
             <div className='flex flex-col rounded-lg bg-[#FFFFFF] min-h-64 mt-8 mb-8 shadow hover:shadow-lg'>
                 <label className="font-['Montserrat'] text-[1rem] px-8 py-5 font-bold overflow-hidden whitespace-pre-line">Announcement</label>
                 <label className="font-['Montserrat'] text-[1rem] px-8 py-2 overflow-hidden whitespace-pre-line">Total: {data.expand.announcement? data.expand.announcement.length : 0} announcement(s)</label>
+                {
+                    pb.authStore.model!.role.includes('instructor') && <AnnouncementAdder />
+                }
                 <div className="flex flex-col px-8 py-4 max-h-[20rem] overflow-auto">
                     <table className="table-fixed">
                         <thead>
