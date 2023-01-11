20f5e566aea4a09c4476b7f80275f0d48fe76877
creampiney <sorawit12535@gmail.com>
2023-01-11 21:10:28 +0700

    feat: delete and edit materials

--- frontend-tsc/src/components/Course/Material.tsx
+++ frontend-tsc/src/components/Course/Material.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import PocketBase from 'pocketbase';
 import MaterialAdder from './Adder/MaterialAdder';
+import moment from 'moment'
+import Swal from 'sweetalert2';
 
 const pb = new PocketBase('https://pb.jjus.dev');
 
@@ -13,9 +15,39 @@ const Material = ({data}:{data:any;}) => {
     if (data.expand.material) {
         trData = data.expand.material.sort(function compareFn(a:any, b:any) {return (new Date(b.created).valueOf() - new Date(a.created).valueOf())}).map((materialData : any, index: number) => {
             return (
-                <tr key={index} className="cursor-pointer hover:bg-[#F6F5F4]" onClick={async () => {const url = pb.getFileUrl(await pb.collection('materials').getOne(materialData.id), materialData.file); window.location.href = url;}}>
-                    <td className="px-4 py-2">{materialData.name}</td>
-                    <td className="px-4 py-2">{(new Date(materialData.created)).toLocaleString()}</td>
+                <tr key={index} className="cursor-pointer hover:bg-[#F6F5F4]">
+                    <td className="px-4 py-2" onClick={async () => {const url = pb.getFileUrl(await pb.collection('materials').getOne(materialData.id), materialData.file); window.location.href = url;}}>{materialData.name}</td>
+                    <td className="px-4 py-2" onClick={async () => {const url = pb.getFileUrl(await pb.collection('materials').getOne(materialData.id), materialData.file); window.location.href = url;}}>{(new Date(materialData.created)).toLocaleString()}</td>
+                    {pb.authStore.model!.role.includes('instructor') && 
+                    <td className="px-4 py-2">
+                        <div className="flex items-center justify-center gap-x-2">
+                            <button className="bg-[#A95151] hover:bg-red-700 text-[0.8rem] text-white font-bold py-2 px-2 rounded-full focus:outline-none focus:shadow-outline" type="button" onClick={() => {
+                                Swal.fire({
+                                  title: 'Do you want to delete this material?',
+                                  showDenyButton: true,
+                                  confirmButtonText: 'Delete',
+                                }).then(async (result) => {
+                                  /* Read more about isConfirmed, isDenied below */
+                                  if (result.isConfirmed) {
+                                    await pb.collection('materials').delete(materialData.id)
+                                    .then(() => Swal.fire({
+                                      
+                                        icon: 'success',
+                                        title: 'This material is deleted!',
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                      }).then(() => {window.location.reload()}))
+                                  }
+                                })}}>
+                                Delete
+                            </button>
+                            <button className="bg-[#5996A5] hover:bg-blue-700 text-[0.8rem] text-white font-bold py-2 px-2 rounded-full focus:outline-none focus:shadow-outline" type="button" onClick={() => {navigate('/courses/'+data.id+'/materials/'+materialData.id+'/edit')}}>
+                                Edit
+                            </button>
+                        </div>
+                    </td>
+                        
+                    }
                 </tr>
             )
         });
@@ -32,8 +64,9 @@ const Material = ({data}:{data:any;}) => {
                     <table className="table-fixed">
                         <thead>
                             <tr>
-                                <th className="w-[70%] px-4 py-2">Name</th>
-                                <th className="w-[30%] px-4 py-2">Date</th>
+                                <th className={pb.authStore.model!.role.includes('instructor') ? "w-[60%] px-4 py-2" : "w-[70%] px-4 py-2"}>Name</th>
+                                <th className={pb.authStore.model!.role.includes('instructor') ? "w-[20%] px-4 py-2" : "w-[30%] px-4 py-2"}>Date</th>
+                                {pb.authStore.model!.role.includes('instructor') && <th className="w-[20%] px-4 py-2">Operation</th>}
                             </tr>
                         </thead>
                         <tbody>{trData}</tbody>
