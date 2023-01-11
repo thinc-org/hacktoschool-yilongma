e12a4b32a454c1d03bb652984c4b6d3b606a2a09
JustinJira <jus.jira@gmail.com>
2023-01-11 13:53:55 +0700

    feat: pretty date formats

--- frontend-tsc/src/components/Announcement/AnnouncementBox.tsx
+++ frontend-tsc/src/components/Announcement/AnnouncementBox.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react'
 import PocketBase from 'pocketbase'
 import { useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import moment from 'moment'
 
 const pb = new PocketBase('https://pb.jjus.dev');
 
@@ -42,7 +43,7 @@ const AnnouncementBox = () => {
             <div className='min-w-full flex flex-col md:p-12 md:px-24 lg:p-12 lg:px-48  xl:p-12 xl:px-48'>
                 <div className='flex flex-col rounded-lg bg-[#FFFFFF] min-h-64 mt-8 mb-8 shadow hover:shadow-lg'>
                     <label className="font-['Montserrat'] text-[1.2rem] px-8 py-5 font-bold overflow-hidden whitespace-pre-line">{announcementData.name}</label>
-                    <label className="font-['Montserrat'] text-[1rem] px-8 py-5 font-bold overflow-hidden whitespace-pre-line">Created on: {(new Date(announcementData.created)).toLocaleString()}</label>
+                    <label className="font-['Montserrat'] text-[1rem] px-8 py-5 font-bold overflow-hidden whitespace-pre-line">Created on: {(new Date (announcementData.created).toLocaleString())}</label>
                     <label className="font-['Montserrat'] text-[1rem] px-8 py-4 overflow-hidden whitespace-pre-line">{announcementData.description}</label>
                 </div>
             </div>
