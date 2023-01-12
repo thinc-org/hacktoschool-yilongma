56b0af2f784b31408060f33225c6e6cbef77f2dd
JustinJira <jus.jira@gmail.com>
2023-01-12 23:07:53 +0700

    fix: tags color are now showing

--- frontend-tsc/src/components/Tags/Tag.tsx
+++ frontend-tsc/src/components/Tags/Tag.tsx
@@ -2,9 +2,12 @@ const Tag = ({ name, bgColor, textColor }: { name: string; bgColor: string; text
 
 
     return (
-        <div className={`bg-[${bgColor}] px-2 py-1 rounded-full`}><span className={`text-[${textColor}] text-[0.8rem]`}>{name}</span></div>
+        <>
+            <div style={{backgroundColor: `${bgColor}`}} className={'px-2 py-1 rounded-full'}>
+                <span style={{color: `${textColor}`}} className={'text-[0.8rem]'}>{name}</span>
+            </div>
+        </>
     )
 }
-
 
 export default Tag
