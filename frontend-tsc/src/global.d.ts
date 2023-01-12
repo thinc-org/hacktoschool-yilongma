494243279d48b2b89f7476a116f48e1dd3302336
JustinJira <jus.jira@gmail.com>
2023-01-12 15:34:11 +0700

    refactor: package and configs

---
+++ frontend-tsc/src/global.d.ts
@@ -0,0 +1,12 @@
+declare module "*&imagetools" {
+    /**
+     * actual types
+     * - code https://github.com/JonasKruckenberg/imagetools/blob/main/packages/core/src/output-formats.ts
+     * - docs https://github.com/JonasKruckenberg/imagetools/blob/main/docs/guide/getting-started.md#metadata
+     */
+    const out: any;
+    export default out;
+  }
+
+/// <reference types="vite/client" />
+/// <reference path="global.d.ts" />
