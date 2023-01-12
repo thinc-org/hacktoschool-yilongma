982836071875cfc3d0fe91356425ce9ab722666a
creampiney <sorawit12535@gmail.com>
2023-01-12 23:14:32 +0700

    feat: courses tag in mobile

--- frontend-tsc/src/components/CoursesList/CourseBox.tsx
+++ frontend-tsc/src/components/CoursesList/CourseBox.tsx
@@ -57,13 +57,19 @@ const CourseBox = ({ id, name, instructor, data, tag }: { id: string; name: stri
                         <div className='relative flex flex-col flex-shrink min-w-0 items-center md:items-start px-4 py-8 md:px-8 md:py-4 gap-2 md:gap-6'>
                             <p className="font-['DelaGothicOne'] text-sm md:text-[1.2rem] overflow-hidden whitespace-pre-line">{name}</p>
                             <p className="font-['Montserrat'] text-sm md:text-[1rem] font-bold overflow-hidden whitespace-pre-line">Instructor: {instructor}</p>
-                            <div className="font-['Montserrat'] text-sm md:text-[1rem] font-bold overflow-hidden whitespace-pre-line">
+                            <div className="flex flex-row max-w-full font-['Montserrat'] text-sm md:text-[1rem] font-bold whitespace-pre-line flex-wrap gap-1">
                             {   
                                 pb.authStore.model!.role.includes('student') && 
                                 (data.student.includes(pb.authStore.model!.id) ? 
-                                <Tag name="Enrolled" bgColor="#d1feb6" textColor="#4b991c" /> 
+                                <Tag name="Enrolled" bgColor="#d1feb6" textColor="#4b991c" />
                                 : 
                                 <Tag name="Unenrolled" bgColor="#ffe2bf" textColor="#d67f19" />)
+                            }
+                            {
+                                (data.expand.tag) &&
+                                (data.expand.tag.map((data: any, index: number) => {
+                                    return <Tag key={index} name={data.name} bgColor="#daeffe" textColor="#0c5a93" />
+                                }))
                             }
                             </div>
                             <button className="bg-[#639B6D] hover:bg-[#74bf81] w-[80%] md:w-32 text-white font-bold py-2 rounded focus:outline-none focus:shadow-outline mt-auto md:mt-0" onClick={routeChange}>View</button>
