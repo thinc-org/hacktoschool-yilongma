b3d9116c277350193305a39dea86fecb63ebb124
JustinJira <jus.jira@gmail.com>
2023-01-13 00:16:00 +0700

    chore: config

--- frontend-tsc/vite.config.ts
+++ frontend-tsc/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { imagetools } from 'vite-imagetools'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import mkcert from 'vite-plugin-mkcert'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), imagetools()],
+  plugins: [react(), imagetools(), mkcert()],
+  mode: "development",
+  build: {
+    minify: true,
+  }
 })
