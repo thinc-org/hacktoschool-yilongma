b82c749cd951cc71dc92be85c93eaeecf9a8c441
creampiney <sorawit12535@gmail.com>
2023-01-12 12:13:40 +0700

    feat: implement profile page

---
+++ frontend-tsc/src/components/Profile/Profile.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+const Profile = () => {
+
+    return (
+        <div className='max-w-screen min-h-screen bg-[#F6F5F4] flex flex-col items-center md:items-start'>
+            <div className='min-w-full flex flex-col md:p-12 md:px-24 lg:p-12 lg:px-48  xl:p-12 xl:px-48'>
+                
+                
+            </div>
+        </div>
+    )
+
+}
+
+export default Profile
