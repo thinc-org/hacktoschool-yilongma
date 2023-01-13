70e008dc04a7d2140d54f13cd53f7777c57cffef
JustinJira <jus.jira@gmail.com>
2023-01-13 18:14:09 +0700

    test: testing payments

--- frontend-tsc/src/components/Payment.tsx
+++ frontend-tsc/src/components/Payment.tsx
@@ -1,10 +1,14 @@
-import getStripe from './Variables/getStripe';
+import PocketBase from 'pocketbase'
+const pb = new PocketBase('https://pb.jjus.dev');
+
 export default function Payment() {
+    var userId = pb.authStore.model!.id
+    var link = "https://stripe.jjus.dev/create-checkout-session?name=Test 1&course=r4sagx5tir37z4l&price=100&user=" + userId
 
 
   return (
     <div>
-        <form action="http://localhost:4242/create-checkout-session" method='POST'>
+        <form action={link} method='POST'>
             <button type='submit'>Checkout</button>
         </form>
     </div>
