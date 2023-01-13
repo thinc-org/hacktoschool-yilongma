9a75da7e875a9c93c40ea51e404a79ccb2ec4c2b
JustinJira <jus.jira@gmail.com>
2023-01-13 15:45:44 +0700

    test: testing payments

---
+++ frontend-tsc/src/components/Variables/getStripe.tsx
@@ -0,0 +1,11 @@
+import { loadStripe, Stripe } from '@stripe/stripe-js';
+
+let stripePromise: Promise<Stripe | null>;
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
+  }
+  return stripePromise;
+};
+
+export default getStripe;
